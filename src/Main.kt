2ade7882c05966c29a9d1654251e78128e51ae63
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2024-10-22 21:33:37 +0300

    учу котлин

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+fun calcTax(sum: Int = 1000): Double {
+    val tax_ratio = 0.13
+
+    return tax_ratio * sum.toDouble()
+}
+
+fun main() {
+    val sum_user: String? = readLine()  //  или  val sum_user = readLine()!!.toInt()
+
+    if (sum_user == null) return
+
+    val sum: Int = sum_user.toInt()
+    val tax: Double = calcTax(sum)
+
+    println("For $sum tax is $tax")
+}
