95ece9c29ecbc17eafbbcf2883c6b79b07fda204
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2024-11-18 22:04:08 +0300

    исключения

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,17 @@
-fun main() {
+fun main(args: Array<String>) {
+
+    val str:String? = readLine()
+
+    if (str == null) return
+
+    val list_of_numbers = str.split( ",")
+
+    var sum: Int = 0
+
+    for (number in list_of_numbers) {
+
+        sum += try { number.toInt() } catch (e: NumberFormatException) {0}
+
+    }
+    println("Sum = ${sum}")
 }
