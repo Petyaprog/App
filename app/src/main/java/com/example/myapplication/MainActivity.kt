da8f773c5ac424b4c440ad92cee512ce3c27a740
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-03 17:40:04 +0300

    Целочисленный калькулятор

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,34 +1,46 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var but1: Button
-    private lateinit var tw: TextView
-    private lateinit var et: EditText
+    private lateinit var add: Button
+    private lateinit var subtr: Button
+    private lateinit var mul: Button
+    private lateinit var divide: Button
+    private lateinit var answer: TextView
+    private lateinit var arg1: EditText
+    private lateinit var arg2: EditText
 
-    private fun button_click_handler(v: View) {
-        val inputText = et.text.toString()
-        tw.text = inputText
-    }
+    private fun calc(operation: ((Int, Int) -> Int)) =
+        answer.setText(
+            try{
+                operation(arg1.text.toString().toInt(), arg2.text.toString().toInt()).toString()
+        } catch (e: NumberFormatException) {"Input Error"}
+            catch(e: ArithmeticException) {"Div by zero"}
+        )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        but1 = findViewById(R.id.button1)
-        tw = findViewById(R.id.textView)
-        et = findViewById(R.id.editTextText)
+        add = findViewById(R.id.add)
+        subtr = findViewById(R.id.subtr)
+        mul = findViewById(R.id.mul)
+        divide = findViewById(R.id.divide)
+        answer = findViewById(R.id.answer)
+        arg1 = findViewById(R.id.arg1)
+        arg2 = findViewById(R.id.arg2)
 
-        but1.setOnClickListener(this::button_click_handler)
-
+        add.setOnClickListener { calc {a, b -> a + b}}
+        subtr.setOnClickListener { calc {a, b -> a - b}}
+        mul.setOnClickListener { calc { a, b -> a * b }}
+        divide.setOnClickListener { calc { a, b -> a / b }}
     }
 }
