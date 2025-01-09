22dbb7674a92cc6c38b270f48c1334e9a0446a07
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-09 14:39:36 +0300

    Дюймы <-> километры

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,98 +3,78 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.os.Bundle
-import android.widget.Button
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import android.graphics.Color
-import android.widget.ProgressBar
-import android.widget.TextView
-import java.util.Timer
-import java.util.TimerTask
-
-enum class TaskState {
-    FREE,
-    PRE_WORKING,
-    CONNECTING,
-    POST_WORKING,
-    COMPLETED
-}
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var but: Button
-    private lateinit var tw: TextView
-    private lateinit var bar: ProgressBar
-    private lateinit var timer: Timer
-    private var taskProgress = 0
-    private var taskState: TaskState = TaskState.FREE
+    private lateinit var status: TextView
+    private lateinit var editText: EditText
+    private lateinit var editText2: EditText
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("SetTextI18n")
+    private fun convert(Flag: Boolean) {
+        val inchesInKilometer = 39370
+        if (Flag) {
+            val inchesOfString: String = editText.text.toString()
+            val inchesOfFloat: Float? = inchesOfString.toFloatOrNull()
+            if (inchesOfFloat != null) {
+                status.text = ""
+                var result: Float = inchesOfFloat / inchesInKilometer
+                editText2.setText(result.toString())
+            } else { status.text = "error" }
+        } else {
+            val kilometersOfString: String = editText2.text.toString()
+            val kilometersOfFloat: Float? = kilometersOfString.toFloatOrNull()
+            if (kilometersOfFloat != null) {
+                status.text = ""
+                val inches: Float = kilometersOfFloat * inchesInKilometer
+                editText.setText(inches.toString())
+            } else { status.text = "error" }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        but = findViewById(R.id.button)
-        tw = findViewById(R.id.textView2)
-        bar = findViewById(R.id.progressBar)
-        timer = Timer()
+        status = findViewById(R.id.textView)
+        editText = findViewById(R.id.editTextText)
+        editText2 = findViewById(R.id.editTextText2)
 
-        but.setOnClickListener {
-            timer.scheduleAtFixedRate(object : TimerTask() {
-                override fun run() {
-                    when (taskState) {
-                        TaskState.FREE -> {
-                            taskState = TaskState.PRE_WORKING
-                            ++taskProgress
+        editText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
 
-                            tw.text = "Working..."
-                            bar.progress = taskProgress
-                        }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
 
-                        TaskState.PRE_WORKING -> {
-                            if (taskProgress < 50) {
-                                ++taskProgress
-
-                                bar.progress = taskProgress
-                            } else {
-                                taskState = TaskState.CONNECTING
-
-                                tw.text = "Connecting to server..."
-
-                                bar.isIndeterminate = true
-
-                                Thread.sleep(5000)
-
-                            }
-                        }
-
-                        TaskState.CONNECTING -> {
-                            taskState = TaskState.POST_WORKING
-                            ++taskProgress
-
-                            tw.text = "Working..."
-                            bar.isIndeterminate = false
-                            bar.progress = taskProgress
-                        }
-
-                        TaskState.POST_WORKING -> {
-                            if (taskProgress < 100) {
-                                ++taskProgress
-
-                                bar.progress = taskProgress
-                            } else {
-                                tw.text = "Completed!"
-                                taskState = TaskState.COMPLETED
-                            }
-                        }
-
-                        TaskState.COMPLETED -> {
-                            timer.cancel()
-                        }
-                    }
+            override fun afterTextChanged(s: Editable?) {
+                if (editText.isFocused) {
+                    // true - 1st editText, false - 2nd editText
+                    convert(true)
                 }
-            }, 1000, 250)
-        }
+            }
+        })
+
+        editText2.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (editText2.isFocused) {
+                    // true - 1st editText, false - 2nd editText
+                    convert(false)
+                }
+            }
+        })
     }
 }
