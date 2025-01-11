983aa4be37c30862abae7d2e3d4beec226b6ab72
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-11 18:24:12 +0300

    проверка ответа на вопрос с выбором ответов

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,76 +5,35 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var status: TextView
-    private lateinit var editText: EditText
-    private lateinit var editText2: EditText
-
-    @SuppressLint("SetTextI18n")
-    private fun convert(Flag: Boolean) {
-        val inchesInKilometer = 39370
-        if (Flag) {
-            val inchesOfString: String = editText.text.toString()
-            val inchesOfFloat: Float? = inchesOfString.toFloatOrNull()
-            if (inchesOfFloat != null) {
-                status.text = ""
-                var result: Float = inchesOfFloat / inchesInKilometer
-                editText2.setText(result.toString())
-            } else { status.text = "error" }
-        } else {
-            val kilometersOfString: String = editText2.text.toString()
-            val kilometersOfFloat: Float? = kilometersOfString.toFloatOrNull()
-            if (kilometersOfFloat != null) {
-                status.text = ""
-                val inches: Float = kilometersOfFloat * inchesInKilometer
-                editText.setText(inches.toString())
-            } else { status.text = "error" }
-        }
-    }
+    private lateinit var butAnswer: Button
+    private lateinit var tvAnswer: TextView
+    private lateinit var rgAnswers: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        status = findViewById(R.id.textView)
-        editText = findViewById(R.id.editTextText)
-        editText2 = findViewById(R.id.editTextText2)
+        butAnswer = findViewById(R.id.butAnswer)
+        tvAnswer = findViewById(R.id.tvAnswer)
+        rgAnswers = findViewById(R.id.rgAnswers)
 
-        editText.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
+        butAnswer.setOnClickListener{
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
+            if (rgAnswers.checkedRadioButtonId == R.id.rbEllipsoid) tvAnswer.text = "Правильно"
+            else tvAnswer.text = "Неправильно"
+        }
 
-            override fun afterTextChanged(s: Editable?) {
-                if (editText.isFocused) {
-                    // true - 1st editText, false - 2nd editText
-                    convert(true)
-                }
-            }
-        })
-
-        editText2.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if (editText2.isFocused) {
-                    // true - 1st editText, false - 2nd editText
-                    convert(false)
-                }
-            }
-        })
     }
 }
