b601ba9072f3f2555a4a9dc177c658fd0ddc3bd9
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-12 16:00:35 +0300

    крестики нолики(не доделал)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,65 +1,41 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.RadioButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var Text: EditText
-    private lateinit var stats: TextView
-    private lateinit var word_count: TextView
-    private lateinit var save: Button
-    private lateinit var load: Button
-    private lateinit var clear: Button
+    private lateinit var allSpinners: Array<Array<Spinner>>
+    val toChooseList: List<String> = listOf(" ", "X", "0")
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Text = findViewById(R.id.editText)
-        stats = findViewById(R.id.stats_view)
-        word_count = findViewById(R.id.unsaved_changes_view)
-        save = findViewById(R.id.save_button)
-        load = findViewById(R.id.load_button)
-        clear = findViewById(R.id.clear_button)
+        allSpinners = Array(4) { Array(4) { Spinner(this) } }
 
-        var saved_text: String? = null
-        Text.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        allSpinners[1][1] = findViewById(R.id.spinner1)
+        allSpinners[1][2] = findViewById(R.id.spinner2)
+        allSpinners[1][3] = findViewById(R.id.spinner3)
+        allSpinners[2][1] = findViewById(R.id.spinner4)
+        allSpinners[2][2] = findViewById(R.id.spinner5)
+        allSpinners[2][3] = findViewById(R.id.spinner6)
+        allSpinners[3][1] = findViewById(R.id.spinner7)
+        allSpinners[3][2] = findViewById(R.id.spinner8)
+        allSpinners[3][3] = findViewById(R.id.spinner9)
 
-            override fun afterTextChanged(s: Editable?) {
-                val s = Text.text.toString().split("[^a-zA-Zа-яА-я0-9_]+".toRegex()).filter { it != "" }
-                word_count.text = "${s.size}"
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, toChooseList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-                if (Text.text.toString() != saved_text)
-                    stats.text = "Unsaved changes"
-                else stats.text = "All changes saved"
+        for (i in 1..3) {
+            for (j in 1..3) {
+                allSpinners[i][j].setAdapter(adapter)
             }
-        })
-
-        save.setOnClickListener {
-            saved_text = Text.text.toString()
-            stats.text = "All changes saved"
         }
 
-        load.setOnClickListener {
-            Text.setText("$saved_text")
-        }
-
-        clear.setOnClickListener {
-            Text.setText("")
-        }
     }
 }
