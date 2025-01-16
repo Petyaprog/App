97c8eefa903db1c7c12b78ddf9b94746c2ee6820
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-16 22:43:31 +0300

    Работа с ListView и SeekBar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,40 +1,54 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.ListView
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var but: Button
-    private lateinit var tw: TextView
+    private lateinit var listView: ListView
+    private lateinit var seekBar: SeekBar
+    private lateinit var adapter: ArrayAdapter<String>
+    private val squaresList = mutableListOf<String>()
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        but = findViewById(R.id.button)
+        listView = findViewById(R.id.listView)
+        seekBar = findViewById(R.id.seekBar)
 
-        but.setOnClickListener {
-            val myintent = Intent(this, MainActivity2::class.java)
-            startActivityForResult(myintent, 1)
-        }
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, squaresList)
+        listView.adapter = adapter
+
+        // Установка слушателя на SeekBar
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateList(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Не требуется
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Не требуется
+            }
+        })
     }
 
-    @Deprecated("Deprecated in Java")
-    @SuppressLint("MissingSuperCall", "SetTextI18n")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        tw = findViewById(R.id.textView)
-        if (data == null) return
-        when(requestCode) {
-            1 -> tw.text = data.getStringExtra("name")
-            else -> tw.text = "Different request code $requestCode"
+    private fun updateList(value: Int) {
+        squaresList.clear() // Очищаем текущий список
+        if (value > 0) {
+            for (i in 1..value) {
+                squaresList.add((i * i).toString()) // Добавляем квадрат числа
+            }
         }
+        adapter.notifyDataSetChanged() // Обновляем адаптер
     }
 }
