c5870eedb9454b238eb0fbbbcd9df0de31dbe8b0
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-18 17:30:47 +0300

    Работа с GridView

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.GridView
 import android.widget.ListView
 import android.widget.ScrollView
 import android.widget.SeekBar
@@ -14,19 +17,23 @@ import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var textView: TextView
+    private lateinit var gridView: GridView
 
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.textView)
-
-        // Длинный текст для прокрутки
-        val longText = StringBuilder()
-        for (i in 1..50) {
-            longText.append("Это строка номер $i\n")
+        gridView = findViewById(R.id.gridview)
+        val numbers = MutableList(31, { "$it" })
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numbers)
+        gridView.adapter = adapter
+        gridView.columnWidth = 100
+        gridView.numColumns = 4
+        gridView.setOnItemClickListener { parent: AdapterView<*>, view: View, position: Int, id: Long ->
+            numbers.removeAt(position)
+            adapter.notifyDataSetChanged()
         }
-        textView.text = longText.toString()
+
     }
 }
