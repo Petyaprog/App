4088460669b8a79bc1cf629e6a08ff598148e2b1
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-19 22:56:00 +0300

    Работа с разрешениями

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,39 +1,56 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.GridView
 import android.widget.ListView
 import android.widget.ScrollView
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var gridView: GridView
+    private val CAMERA_REQUEST_CODE = 100
 
     @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gridView = findViewById(R.id.gridview)
-        val numbers = MutableList(31, { "$it" })
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numbers)
-        gridView.adapter = adapter
-        gridView.columnWidth = 100
-        gridView.numColumns = 4
-        gridView.setOnItemClickListener { parent: AdapterView<*>, view: View, position: Int, id: Long ->
-            numbers.removeAt(position)
-            adapter.notifyDataSetChanged()
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            //use camera
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
         }
 
+
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){}
+            }
+        }
     }
 }
