7b6ef42f4aa1122b5a30d155393c2523eb75337e
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2025-01-26 13:47:09 +0300

    Работа с двумя Activity через уведомление

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.core.app.NotificationCompat
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var button: Button
+    private lateinit var number: EditText
+    private lateinit var message: EditText
 
     @SuppressLint("MissingInflatedId", "SetTextI18n", "PrivateResource")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,31 +24,34 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.button)
+        number = findViewById(R.id.number1)
+        message = findViewById(R.id.message1)
 
         val channelId = "my_channel_id"
         val channelName = "My Channel"
 
-        // Создание NotificationManager
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = getSystemService(NotificationManager::class.java)
 
-        // Создание канала уведомлений (для Android 8.0 и выше)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
         }
 
         button.setOnClickListener {
-            // Создание уведомления
-            val notification = NotificationCompat.Builder(this, channelId)
-                .setContentTitle("Заголовок уведомления")
-                .setContentText("Текст уведомления")
-                .setSmallIcon(android.R.drawable.sym_def_app_icon) // Замените на ваш иконку
-                .setAutoCancel(true) // Уведомление исчезнет при нажатии
-                .build()
+            val intent = Intent(this, MainActivity2::class.java).apply {
+                putExtra("number", number.text.toString())
+                putExtra("message", message.text.toString())
+            }
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
 
-            // Отправка уведомления
+            val notification = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setAutoCancel(true)
+                .setContentTitle("Заголовок")
+                .setContentText("Переход")
+                .setContentIntent(pendingIntent)
+                .build()
             notificationManager.notify(1, notification)
         }
     }
-
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,21 +1,28 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity2 : ComponentActivity() {
 
+    private lateinit var number2: TextView
+    private lateinit var message2: TextView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        number2 = findViewById(R.id.number)
+        message2 = findViewById(R.id.message)
+
+        val number = intent.getStringExtra("number") ?: "Не указано"
+        val message = intent.getStringExtra("message") ?: "Не указано"
+
+        // Устанавливаем данные в TextView
+        this.number2.text = "Число: $number"
+        this.message2.text = "Сообщение: $message"
     }
 }
